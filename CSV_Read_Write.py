5d2934d055f289add1a2dc187c219022b74afa4c
MateuszKow <72598405+MateuszKow@users.noreply.github.com>
2022-04-08 21:19:56 +0200

    CSV_Read updated to add date of set

--- CSV_Read_Write.py
+++ CSV_Read_Write.py
@@ -3,28 +3,25 @@ import csv
 
 
 class CSVReadWrite:
-    def write(in_coming_str: str):
+    def write(self,in_coming_str: str):
         list_of_pills = in_coming_str.split(",")
         with open('pills_storage.csv', 'w') as file:
             writer = csv.writer(file)
             writer.writerow(list_of_pills)
 
-
-    def read():
+    def read(self):
         pills = pd.read_csv("pills_storage.csv", header=None)
         pills = pills.values.tolist()[0]
         return pills
 
-
-    def write_set_of_pills(in_coming_str: str):
+    def write_set_of_pills(self,in_coming_str, date):
         sets_pills = in_coming_str.split(",")
+        sets_pills.insert(0, date)
         with open('pills_sets.csv', 'a', newline="") as file:
             writer = csv.writer(file)
             writer.writerow(sets_pills)
 
-
-    def read_set_of_pills():
+    def read_set_of_pills(self):
         pills = pd.read_csv("pills_sets.csv", header=None)
         pills = pills.values.tolist()
         return pills
-

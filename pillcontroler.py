3fe372512bade511bee4371a5e24bdbc9c2fda6a
Jakub Mazur <kubam0906@gmail.com>
2022-04-08 19:55:05 +0200

    PillControler PillBox refactor

--- pillcontroler.py
+++ pillcontroler.py
@@ -2,17 +2,30 @@ import maestro
 from time import sleep
 
 
-class PillControler:
+class PillBox:
+    def __init__(self, controller, servo_no):
+        self.controller = controller
+        self.servo_no = servo_no
+
+        self.zero()
+
+    def zero(self):
+        self.controller.setTarget(self.servo_no, 1000 * 4)
+
+    def take_pill(self):
+        self.controller.setTarget(self.servo_no, 3000 * 4)
+        sleep(1)
+        self.controller.setTarget(self.servo_no, 1000 * 4)
+
+
+class PillController:
     def __init__(self, tty_str):
         self.servo_controller = maestro.Controller(ttyStr=tty_str)
 
         self.pills_containers_num = 6
+        self.pill_boxes = []
 
-        # zerowanie
-        for i in range(6):
-            self.servo_controller.setTarget(i, 1000*4)
+        for i in range(self.pills_containers_num):
+            self.pill_boxes.append(PillBox(self.servo_controller, i))
 
-    def take_pill(self, pill_type_no):
-        self.servo_controller.setTarget(pill_type_no, 3000*4)
-        sleep(1)
-        self.servo_controller.setTarget(pill_type_no, 1000*4)
+

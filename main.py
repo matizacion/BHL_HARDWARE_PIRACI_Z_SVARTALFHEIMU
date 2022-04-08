4adef04e8d17e8eb1e4319fc3267eb4351c50752
Jakub Mazur <kubam0906@gmail.com>
2022-04-09 00:42:59 +0200

    nothing
    nothing

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from pillcontroler import PillControler
+from pillcontroler import PillController
 from maestro import Controller
 
 if __name__ == '__main__':
-    pc = PillControler(tty_str='COM5')
-    pc.take_pill(1)
+    pc = PillController(tty_str='COM5')
+
+    pc.pill_boxes[5].drop_pill()
     # con = Controller('COM5')
     # print("start")
     # con.setTarget(1, 1500)

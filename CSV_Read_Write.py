6fc795124d0d03b9e75bb01cb72cb53c21cabe42
MateuszKow <72598405+MateuszKow@users.noreply.github.com>
2022-04-09 04:16:04 +0200

    CSV_Read_Write.py -> init and del update

--- CSV_Read_Write.py
+++ CSV_Read_Write.py
@@ -1,10 +1,40 @@
 import pandas as pd
 import csv
 
+data = b'H,6,23,31,5,5'
+
+days = {1: "Mon", 2: "Tue", 3: "Wed", 4: "Thu", 5: "Fri", 6: "Sat", 7: "Sun"}
+
 
 class CSVReadWrite:
-    def write(self,in_coming_str: str):
-        list_of_pills = in_coming_str.split(",")
+    def __init__(self):
+        self.sets_file = open("pills_sets.csv", "r")
+        self.sets = [line.rstrip() for line in self.sets_file]
+        self.sets_file.close()
+        self.sets_file = open("pills_sets.csv", "w", newline='')
+
+        print(self.sets)
+
+        self.pills_file = open("pills_storage.csv", "r")
+        self.pills = [line.rstrip() for line in self.pills_file]
+        self.pills_file.close()
+        self.pills_file = open("pills_storage.csv", "w", newline='')
+
+        print(self.pills)
+
+    def __del__(self):
+
+        for line in self.sets:
+            self.sets_file.write(line)
+            self.sets_file.write('\n')
+
+        for line in self.pills:
+            self.pills_file.write(line)
+            self.pills_file.write('\n')
+        print("destruktor")
+
+    def write(self, incoming_str):
+        list_of_pills = incoming_str.split(",")
         with open('pills_storage.csv', 'w') as file:
             writer = csv.writer(file)
             writer.writerow(list_of_pills)
@@ -14,12 +44,17 @@ class CSVReadWrite:
         pills = pills.values.tolist()[0]
         return pills
 
-    def write_set_of_pills(self,in_coming_str, date):
-        sets_pills = in_coming_str.split(",")
-        sets_pills.insert(0, date)
-        with open('pills_sets.csv', 'a', newline="") as file:
-            writer = csv.writer(file)
-            writer.writerow(sets_pills)
+    def write_set_of_pills(self, incoming_bytes):
+        incoming_str = incoming_bytes.decode('UTF-8')
+        sets_pills = incoming_str.split(",")
+        date = days[int(sets_pills[1])] + " " + sets_pills[2] + sets_pills[3]
+        empty_list = [0] * 6
+        empty_list[0] = date
+        print(date)
+        # sets_pills.insert(0, date)
+        # with open('pills_sets.csv', 'a', newline="") as file:
+        #     writer = csv.writer(file)
+        #     writer.writerow(sets_pills)
 
     def read_set_of_pills(self):
         pills = pd.read_csv("pills_sets.csv", header=None)
@@ -31,7 +66,4 @@ class CSVReadWrite:
         return pills_dict
 
 
-
-
-
-
+w = CSVReadWrite()

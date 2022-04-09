3d18b2b79fe029f2a1058271dc16b3ad3307e85e
MateuszKow <72598405+MateuszKow@users.noreply.github.com>
2022-04-09 06:52:28 +0200

    Creating documentation

--- CSV_Read_Write.py
+++ CSV_Read_Write.py
@@ -1,7 +1,13 @@
 class CSVReadWrite:
+    """
+    Niezmiennik klasy -> słownik przyporządkujący liczbie porządkowej dzień tygodnia
+    """
     days = {1: "Mon", 2: "Tue", 3: "Wed", 4: "Thu", 5: "Fri", 6: "Sat", 7: "Sun"}
 
     def __init__(self):
+        """
+        konstruktor klasy CSVReadWrite wczytujący dane z pliku csv
+        """
         self.sets_file = open("pills_sets.csv", "r")
         self.sets = [(line.rstrip().split(',')) for line in self.sets_file]
 
@@ -18,6 +24,10 @@ class CSVReadWrite:
         self.pills = self.pills.split(",")
 
     def __del__(self):
+        """
+        Destruktor klasy CSVReadWrite zapisujący dane do pliku csv
+        :return: None
+        """
         for key in self.sets.keys():
             self.sets_file.write(key + "," + ",".join(self.sets[key]))
             self.sets_file.write('\n')
@@ -27,9 +37,19 @@ class CSVReadWrite:
         self.sets_file.close()
 
     def write(self, split_str):
+        """
+        metoda nadpisująca aktualną ilość tabletek
+        :param split_str: List[str] -> każdy element to liczba tabletek odpowiedniego rodzaju
+        :return: None
+        """
         self.pills = split_str
 
     def write_set_of_pills(self, split_str):
+        """
+        metoda dopisująca/uaktualniająca dawki lekarstw na podawanych w określonym czasie
+        :param split_str: List[str] -> pierwszym elementem jest data kolejne to liczby tabletek odpowiedniego rodzaju
+        :return: None
+        """
         date = self.days[int(split_str[0])] + " " + split_str[1] + split_str[2]
         if date in self.sets.keys():
             (self.sets[date])[int(split_str[3])] = split_str[4]
@@ -39,6 +59,11 @@ class CSVReadWrite:
             self.sets[date] = zeros
 
     def which_write(self, incoming_bytes):
+        """
+        metoda sprawdzająca jakiego rodzaju dane są przetwarzane, a następnie wykonuje na nich odpowiednie metody zapisu
+        :param incoming_bytes: przesłane dane w formie bajtowej
+        :return: None
+        """
         incoming_str = incoming_bytes.decode('UTF-8')
         split_str = incoming_str.split(",")
         if split_str[0] == 'S':

62e80dd593583d0ef7ebca83bb2314a6d47e90c0
MateuszKow <72598405+MateuszKow@users.noreply.github.com>
2022-04-09 05:40:44 +0200

    CSV_Read_Write.py -> finished

--- CSV_Read_Write.py
+++ CSV_Read_Write.py
@@ -1,7 +1,9 @@
 import pandas as pd
 import csv
 
-data = b'H,6,23,31,5,5'
+data = b'H,2,10,1,3,10'
+data2 = b'S,2,1,1,7,5'
+data3 = b'H,1,18,30,2,1'
 
 days = {1: "Mon", 2: "Tue", 3: "Wed", 4: "Thu", 5: "Fri", 6: "Sat", 7: "Sun"}
 
@@ -9,61 +11,61 @@ days = {1: "Mon", 2: "Tue", 3: "Wed", 4: "Thu", 5: "Fri", 6: "Sat", 7: "Sun"}
 class CSVReadWrite:
     def __init__(self):
         self.sets_file = open("pills_sets.csv", "r")
-        self.sets = [line.rstrip() for line in self.sets_file]
+        self.sets = [(line.rstrip().split(',')) for line in self.sets_file]
+
+        self.sets = {elem[0]: elem[1:] for elem in self.sets}
+
         self.sets_file.close()
         self.sets_file = open("pills_sets.csv", "w", newline='')
 
-        print(self.sets)
-
         self.pills_file = open("pills_storage.csv", "r")
-        self.pills = [line.rstrip() for line in self.pills_file]
+        self.pills = self.pills_file.read()
         self.pills_file.close()
-        self.pills_file = open("pills_storage.csv", "w", newline='')
 
-        print(self.pills)
+        self.pills_file = open("pills_storage.csv", "w", newline='')
+        self.pills = self.pills.split(",")
 
     def __del__(self):
-
-        for line in self.sets:
-            self.sets_file.write(line)
+        for key in self.sets.keys():
+            self.sets_file.write(key + "," + ",".join(self.sets[key]))
             self.sets_file.write('\n')
 
-        for line in self.pills:
-            self.pills_file.write(line)
-            self.pills_file.write('\n')
+        self.pills_file.write(",".join(self.pills))
+        self.pills_file.close()
+        self.sets_file.close()
         print("destruktor")
 
-    def write(self, incoming_str):
-        list_of_pills = incoming_str.split(",")
-        with open('pills_storage.csv', 'w') as file:
-            writer = csv.writer(file)
-            writer.writerow(list_of_pills)
+    def write(self, split_str):
+        self.pills = split_str
 
-    def read(self):
-        pills = pd.read_csv("pills_storage.csv", header=None)
-        pills = pills.values.tolist()[0]
-        return pills
+    def write_set_of_pills(self, split_str):
+        date = days[int(split_str[0])] + " " + split_str[1] + split_str[2]
+        if date in self.sets.keys():
+            (self.sets[date])[int(split_str[3])] = split_str[4]
+        else:
+            zeros = 6 * ['0']
+            zeros[int(split_str[3])] = split_str[4]
+            self.sets[date] = zeros
 
-    def write_set_of_pills(self, incoming_bytes):
+    def which_write(self, incoming_bytes):
         incoming_str = incoming_bytes.decode('UTF-8')
-        sets_pills = incoming_str.split(",")
-        date = days[int(sets_pills[1])] + " " + sets_pills[2] + sets_pills[3]
-        empty_list = [0] * 6
-        empty_list[0] = date
-        print(date)
-        # sets_pills.insert(0, date)
-        # with open('pills_sets.csv', 'a', newline="") as file:
-        #     writer = csv.writer(file)
-        #     writer.writerow(sets_pills)
+        split_str = incoming_str.split(",")
+        if split_str[0] == 'S':
+            self.write(split_str[1:])
+        elif split_str[0] == 'H':
+            self.write_set_of_pills(split_str[1:])
+        else:
+            pass
 
-    def read_set_of_pills(self):
-        pills = pd.read_csv("pills_sets.csv", header=None)
-        pills = pills.values.tolist()
-        pills_dict = {}
-        for i in range(len(pills)):
-            pills_dict[pills[i][0]] = pills[i][1:]
-
-        return pills_dict
+    # def read_set_of_pills(self):
+    #     pills = pd.read_csv("pills_sets.csv", header=None)
+    #     pills = pills.values.tolist()
+    #     pills_dict = {}
+    #     for i in range(len(pills)):
+    #         pills_dict[pills[i][0]] = pills[i][1:]
+    #
+    #     return pills_dict
 
 
 w = CSVReadWrite()
+w.which_write(data2)
